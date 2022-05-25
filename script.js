d3df3a3b6ece20a949fbb6d4b7b0e053eaa67d8b
sarthak <sarthakbatham26@gmail.com>
2022-05-25 21:21:29 +0530

    split keyword

--- script.js
+++ script.js
@@ -9,13 +9,31 @@ const article = document.querySelector("article");
 function makeOverlappingLetters(text, parity) {
     let htmlString = "";
     for (let i = 0; i < text.length; i++) {
-        if (i % 2 == parity) {
-            htmlString += `<span>${text[i]}</span>`;
-        }
-        else {
-            htmlString += text[i];
-        }
+        htmlString += (i % 2 == parity)
+                    ? `<span>${text[i]}</span>`
+                    : text[i];
     }
     return htmlString;
 }
 
+// Splits the string for the tilt effect
+function splitKeyword(text) {
+    let left = mid = right = "";
+    if (text.length % 2 == 0) {
+        left = text.substr(0, text.length / 2);
+        right = text.substr(text.length / 2);
+    }
+    else {
+        left = text.substr(0, text.length / 2);
+        mid = text.substr(text.length / 2, 1);
+        right = text.substr(text.length / 2 + 1);
+    }
+    return {left, mid, right}
+}
+
+const obj = splitKeyword("PoKabcéMoNxyz")
+const p = document.createElement("p");
+p.innerHTML = `<span class="antiClockwise">${obj.left}</span>`
+            + `<span class="mid">${obj.mid}</span>`
+            + `<span class="clockwise">${obj.right}</span>`;
+article.appendChild(p);
